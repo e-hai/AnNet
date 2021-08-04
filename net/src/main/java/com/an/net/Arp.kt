20a722fa97ab2fc1af02a6ba61844be0171b11db
wanghai <wanghai@qilitech.ltd>
2021-08-04 16:06:20 +0800

    Add ARP and Ping

---
+++ net/src/main/java/com/an/net/Arp.kt
@@ -0,0 +1,100 @@
+package com.an.net
+
+import android.os.Build
+import android.util.Log
+import kotlinx.coroutines.*
+import java.io.File
+import java.io.InputStream
+import java.net.*
+
+
+private fun InputStream.readStreamAsList(): List<String> {
+    return this.bufferedReader()
+        .use {
+            Log.d("ARP", "line=${it.readText()}")
+            it.readLines()
+        }
+}
+
+object Arp {
+    private const val TAG = "ARP"
+
+    suspend fun scan() {
+        Log.d(TAG, "start")
+        sendBroadcast()
+        getArpTable()
+        Log.d(TAG, "end")
+    }
+
+    private suspend fun sendBroadcast() = withContext(Dispatchers.IO) {
+        try {
+            val en = NetworkInterface.getNetworkInterfaces()
+            var host = ""
+            while (en.hasMoreElements()) {
+                val inet = en.nextElement().inetAddresses
+                while (inet.hasMoreElements()) {
+                    inet.nextElement()
+                        .takeIf { !it.isLoopbackAddress && it is Inet4Address }
+                        ?.hostAddress?.let {
+                            host = it.substringBeforeLast(".")
+                        }
+                }
+            }
+            Log.d(TAG, "host=$host")
+            for (i in 0..255) {
+                launch {
+                    val ip = "$host.$i"
+                    val inetAddress = InetAddress.getByName(ip)
+                    inetAddress.isReachable(100)
+                        .apply {
+                            if (this) {
+                                Log.d(
+                                    TAG,
+                                    "$inetAddress ${inetAddress.hostAddress} ${inetAddress.hostName} ${inetAddress.canonicalHostName}"
+                                )
+                            }
+                        }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private suspend fun getArpTable(): List<String> =
+        withContext(Dispatchers.IO) {
+            Log.d(TAG, "getArpTable start")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                getArpTableFromIpCommand()
+            } else {
+                getArpTableFromFile()
+            }
+        }
+
+    private fun getArpTableFromFile(): List<String> {
+        try {
+            return File("/proc/net/arp").inputStream().readStreamAsList().onEach {
+                Log.e(TAG, "arp file = $it")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return emptyList()
+    }
+
+    private fun getArpTableFromIpCommand(): List<String> {
+        try {
+            val execution = Runtime.getRuntime().exec("ip neigh")
+            execution.waitFor()
+            Log.d(TAG, "${execution.exitValue()}")
+            return execution.inputStream.readStreamAsList()
+                .onEach {
+                    Log.e(TAG, "ip neigh= $it")
+                }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return emptyList()
+    }
+}
+
---
+++ net/src/main/java/com/an/net/Ping.kt
@@ -0,0 +1,78 @@
+package com.an.net
+
+import android.util.Log
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.regex.Pattern
+
+object Ping {
+
+    fun ping(address: String, count: Int = 3): PingInfo {
+        Log.d("ping", "start")
+        val process = Runtime.getRuntime().exec("ping -c $count  $address")
+        val pingInfo = PingInfo()
+        BufferedReader(InputStreamReader(process.inputStream)).useLines { result ->
+            result.forEach { line ->
+                Log.d("ping", line)
+                if (line.contains("packets transmitted")) {
+                    parsePacketsTransmitted(line, pingInfo)
+                }
+                if (line.startsWith("rtt")) {
+                    parseRtt(line, pingInfo)
+                }
+            }
+        }
+        Log.d("ping", "pingInfo=$pingInfo")
+
+        return pingInfo
+    }
+
+    private fun parseRtt(line: String, pingInfo: PingInfo) {
+        val startIndex = line.indexOf("=")
+        val endIndex = line.indexOf("ms")
+        line.substring(startIndex, endIndex)
+            .split("/")
+            .forEachIndexed { index, s ->
+                when (index) {
+                    0 -> pingInfo.rttMin = s.toFloat()
+
+                    1 -> pingInfo.rttAvg = s.toFloat()
+
+                    2 -> pingInfo.rttMax = s.toFloat()
+
+                    3 -> pingInfo.rttMdev = s.toFloat()
+                }
+            }
+    }
+
+    private fun parsePacketsTransmitted(line: String, pingInfo: PingInfo) {
+        val pattern = Pattern.compile("[^0-9]")
+        line.split(",").forEach {
+            when {
+                it.contains("packets transmitted") ->
+                    pingInfo.packetsTransmitted =
+                        pattern.matcher(it).replaceAll("").trim().toFloat()
+
+                it.contains("received") ->
+                    pingInfo.packetsReceived = pattern.matcher(it).replaceAll("").trim().toFloat()
+
+                it.contains("packet loss") ->
+                    pingInfo.packetsLoss = pattern.matcher(it).replaceAll("").trim().toFloat()
+
+                it.contains("time") ->
+                    pingInfo.packetsTime = pattern.matcher(it).replaceAll("").trim().toFloat()
+            }
+        }
+    }
+
+    data class PingInfo(
+        var rttMin: Float = 0f,            //发一个包最短时长
+        var rttMax: Float = 0f,            //发一个包最大时长
+        var rttAvg: Float = 0f,            //发包平均时长
+        var rttMdev: Float = 0f,           //包差异时长，值越大，网络越不稳定
+        var packetsTransmitted: Float = 0f,//发送的总包数
+        var packetsReceived: Float = 0f,   //接受了的总包数
+        var packetsLoss: Float = 0f,       //丢失的包百分比
+        var packetsTime: Float = 0f        //总时长
+    )
+}
